eb4e038f9c796fcc1ae2a297856079a0a37eee59
amansawhney <amansawhney@sawhneygames.com>
2017-07-12 16:31:25 -0400

    added another birdie

--- js/app.js
+++ js/app.js
@@ -19,8 +19,29 @@ var motionPath = anime({
     translateX: path('x'),
     translateY: path('y'),
     rotate: path('angle'),
-    easing: 'linear',
-    duration: 2000,
+    easing: 'easeOutCubic',
+    delay: 20000,
+    duration: 4500,
     direction: 'reverse',
     loop: true
 });
+
+
+var duration = anime({
+    targets: '#dummy .el',
+    translateX: 1700,
+    delay: 10000,
+    duration: 9000,
+    easing: 'linear',
+    direction: 'alternate',
+    loop: true,
+    rotate: 540
+
+});
+
+
+// var bird = anime({
+//     targets: '#dummy',
+//     translateX: 100%,
+//     rotate: '1turn'
+// });
